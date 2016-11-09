c15207248dd002ec9f032253d1928694c80200cc
Anna Mishenina <annmishenina@gmail.com>
2016-11-10 00:58:36 +0600

    5

--- src/main/java/formatter/Formatter.java
+++ src/main/java/formatter/Formatter.java
@@ -19,82 +19,93 @@ public class Formatter implements IFormatter {
             int indent = 0;
             boolean onelineComment = false;
             boolean multilineComment = false;
+            boolean charLiteral = false;
+            boolean stringLiteral = false;
             char previousSymbol = ' ';
 
             while (in.ready()) {
                 char symbol = in.readChar();
-
-                switch (symbol) {
-                    case ';':
-                        if (!multilineComment && !onelineComment) {
-                            out.write(symbol);
-                            out.write('\n');
-                            for (int i = 0; i < space * indent; i++) {
-                                out.write(' ');
-                            }
-                        } else {
-                            out.write(symbol);
-                        }
-                        break;
-                    case '{':
-                        if (!multilineComment && !onelineComment) {
-                            indent++;
-                            out.write(' ');
-                            out.write(symbol);
-
-                            out.write('\n');
-                            for (int i = 0; i < space * indent; i++) {
-                                out.write(' ');
-                            }
-
-                        } else {
-                            out.write(symbol);
-                        }
-                        break;
-                    case '}':
-                        if (!multilineComment && !onelineComment) {
-                            indent--;
-                            out.write('\n');
-                            for (int i = 0; i < space * indent; i++) {
-                                out.write(' ');
-                            }
-                            out.write(symbol);
-
-                            out.write('\n');
-                            for (int i = 0; i < space * indent; i++) {
-                                out.write(' ');
-                            }
-
-                        } else {
-                            out.write(symbol);
-                        }
-                        break;
-                    case '/':
-                        if (previousSymbol == '*') {
-                            multilineComment = false;
-                        }
-                        if (previousSymbol == '/') {
-                            onelineComment = true;
-                        }
-                        out.write(symbol);
-                        break;
-                    case '*':
-                        if (previousSymbol == '/') {
-                            multilineComment = true;
-                        }
-                        out.write(symbol);
-                        break;
-                    case '\n':
-                        onelineComment = false;
-                        out.write(symbol);
-                    default:
-                        out.write(symbol);
-                        break;
-
-
+                if (symbol == '\"') {
+                    stringLiteral = !stringLiteral;
                 }
+                if (symbol == '\'') {
+                    charLiteral = !charLiteral;
+                }
+                if (!charLiteral && !stringLiteral) {
+                    switch (symbol) {
+                        case ';':
+                            if (!multilineComment && !onelineComment) {
+                                out.write(symbol);
+                                out.write('\n');
+                                for (int i = 0; i < space * indent; i++) {
+                                    out.write(' ');
+                                }
+                            } else {
+                                out.write(symbol);
+                            }
+                            break;
+                        case '{':
+                            if (!multilineComment && !onelineComment) {
+                                indent++;
+                                out.write(' ');
+                                out.write(symbol);
 
-                previousSymbol = symbol;
+                                out.write('\n');
+                                for (int i = 0; i < space * indent; i++) {
+                                    out.write(' ');
+                                }
+
+                            } else {
+                                out.write(symbol);
+                            }
+                            break;
+                        case '}':
+                            if (!multilineComment && !onelineComment) {
+                                indent--;
+                                out.write('\n');
+                                for (int i = 0; i < space * indent; i++) {
+                                    out.write(' ');
+                                }
+                                out.write(symbol);
+
+                                out.write('\n');
+                                for (int i = 0; i < space * indent; i++) {
+                                    out.write(' ');
+                                }
+
+                            } else {
+                                out.write(symbol);
+                            }
+                            break;
+                        case '/':
+                            if (previousSymbol == '*') {
+                                multilineComment = false;
+                            }
+                            if (previousSymbol == '/') {
+                                onelineComment = true;
+                            }
+                            out.write(symbol);
+                            break;
+                        case '*':
+                            if (previousSymbol == '/') {
+                                multilineComment = true;
+                            }
+                            out.write(symbol);
+                            break;
+                        case '\n':
+                            onelineComment = false;
+                            out.write(symbol);
+                        default:
+                            out.write(symbol);
+                            break;
+
+
+                    }
+
+                    previousSymbol = symbol;
+                } else {
+                    out.write(symbol);
+                }
             }
         } catch (Exception e) {
             throw new FormatterException("format failed", e);
